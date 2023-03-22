4a4d352b9d5ec5d4e36055d519576c12be981273
Mohammad-Azim <mohammad.azim@optimumpartners.co>
2023-03-22 12:07:20 +0200

    create needed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CategoryComponent } from './components/category/category.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { ListMoviesComponent } from './components/list-movies/list-movies.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchComponent } from './components/core/search/search.component';
+import { LogoComponent } from './components/core/logo/logo.component';
+import { NavigationComponent } from './components/core/navigation/navigation.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoryComponent,
+    MovieCardComponent,
+    ListMoviesComponent,
+    FooterComponent,
+    SearchComponent,
+    LogoComponent,
+    NavigationComponent,
+    MovieDetailsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
