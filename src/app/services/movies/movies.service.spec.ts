1cabfe68dbb712aad511e97713aec8205ff71724
Mohammad-Azim <mohammad.azim@optimumpartners.co>
2023-03-27 11:22:44 +0300

    testing service

--- src/app/services/movies/movies.service.spec.ts
+++ src/app/services/movies/movies.service.spec.ts
@@ -1,16 +1,61 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MoviesService } from './movies.service';
+import { ApiKey, MovieAPI } from 'src/app/globalVar';
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(MoviesService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getMovies', () => {
+    it('should call get to return MoviesPage', () => {
+      spyOn(service.http, 'get');
+      service.getMovies(1);
+      expect(service.http.get).toHaveBeenCalledWith(
+        `${MovieAPI}/movie/popular?api_key=${ApiKey}&page=1`
+      );
+    });
+  });
+
+  describe('getMoviesByQuery', () => {
+    it('should call formatQueryString', () => {
+      spyOn(service, 'formatQueryString');
+      service.getMoviesByQuery(1, 'the avengers');
+      expect(service.formatQueryString).toHaveBeenCalledWith('the avengers');
+    });
+
+    it('should call get to return MoviesPage but filtered by search', () => {
+      spyOn(service.http, 'get');
+      service.getMoviesByQuery(1, 'the avengers');
+      expect(service.http.get).toHaveBeenCalledWith(
+        `${MovieAPI}/search/movie?api_key=${ApiKey}&page=1&query=the+avengers`
+      );
+    });
+  });
+
+  describe('getMovieById', () => {
+    it('should call get to get movie by id', () => {
+      spyOn(service.http, 'get');
+      service.getMovieById(32);
+      expect(service.http.get).toHaveBeenCalledWith(
+        `${MovieAPI}/movie/32?api_key=${ApiKey}`
+      );
+    });
+  });
+
+  describe('formatQueryString', () => {
+    it('should re format the search string to be valid to the API', () => {
+      expect(service.formatQueryString(' the avenger ')).toEqual('the+avenger');
+    });
   });
 });
--- src/app/services/movies/movies.service.ts
+++ src/app/services/movies/movies.service.ts
@@ -13,24 +13,45 @@ export class MoviesService {
 
   searchSubject = new Subject<string>();
 
+  /**
+   *
+   * @param pageNumber the number of requested page
+   * @returns MoviesPage type contain list of movies and number of movies
+   */
   getMovies(pageNumber: number): Observable<MoviesPage> {
     return this.http.get<MoviesPage>(
       `${MovieAPI}/movie/popular?api_key=${ApiKey}&page=${pageNumber}`
     );
   }
 
+  /**
+   *
+   * @param pageNumber the number of requested page
+   * @param query the search query that user search on
+   * @returns MoviesPage type contain list of movies that user searching on
+   */
   getMoviesByQuery(pageNumber: number, query: string): Observable<MoviesPage> {
     query = this.formatQueryString(query);
     return this.http.get<MoviesPage>(
-      `${MovieAPI}/search/movie?api_key=f82ecbb7a5110caecaee2bee5e4c79d6&page=${pageNumber}&query=${query}`
+      `${MovieAPI}/search/movie?api_key=${ApiKey}&page=${pageNumber}&query=${query}`
     );
   }
 
+  /**
+   *
+   * @param id id of the movie requested
+   * @returns Movie type contain information about the movie
+   */
   getMovieById(id: number): Observable<Movie> {
     return this.http.get<Movie>(`${MovieAPI}/movie/${id}?api_key=${ApiKey}`);
   }
 
-  private formatQueryString(query: string): string {
+  /**
+   *
+   * @param query the query string (search string user entered)
+   * @returns query string reformated to be valid
+   */
+  formatQueryString(query: string): string {
     query = query.trim();
     let array = query.split(' ');
     query = array.join('+');
